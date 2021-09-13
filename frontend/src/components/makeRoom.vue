88dc5d10a6a5a5a566d9e5ac891404f0b9d48b76
multicampus <ruvan123@naver.com>
2021-09-14 00:23:12 +0900

    [FE] 210913 기본 틀

--- frontend/src/components/makeRoom.vue
+++ frontend/src/components/makeRoom.vue
@@ -2,18 +2,41 @@
   <div>
       <div><h3>make Room</h3></div>
       <div>
+          <label for="">방 이름: </label>
           <input type="text" placeholder="방 제목" value='this.data'>
           <br>
+          <label for=""> 인원수: </label>
           <input type="number" placeholder="인원 제한" value="5" min="0" max="100">
           <br>
+          <label for="">판 수: </label>
           <input type="number" placeholder="몇판" value="6">
+          <br>
+          <label for="">비밀방?</label>
+          <input type="checkbox" @click="togglePassword()">
+          <br>
+          <input type="password">
+          <br>
+          <button>방 만들기</button>
       </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      data: {
+        name: '',
+        number: ''
+      },
+      secret: false
+    }
+  },
+  methods: {
+    togglePassword () {
+      this.secret = !this.secret
+    }
+  }
 }
 </script>
 
