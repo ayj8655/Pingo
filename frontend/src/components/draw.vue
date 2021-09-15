b866ef6ba507092f4f495e28b60f4cd477f82fcd
multicampus <ruvan123@naver.com>
2021-09-15 23:39:08 +0900

    [FE] 210915 make draw.vue

---
+++ frontend/src/components/draw.vue
@@ -0,0 +1,183 @@
+<template>
+  <div>
+    <div>
+        <canvas id="jsCanvas" class="canvas" width="700" height="700" ref="canvas" @mousemove="onMouseMove" @mousedown="onMouseDown" @mouseup="onMouseUp" @mouseleave="stopPainting"></canvas>
+    </div>
+    <div class="controls__range">
+        <input type="range" id="jsRange" v-model="jsRange" min="0.1" max="5" step="0.1">
+    </div>
+    <div class='controls__btns'>
+        <button id='jsMode'>Fill</button>
+        <button id='jsSave'>Save</button>
+    </div>
+    <div class='controls'>
+        <div class='controls__colors' id="jsColors">
+            <div class="controls__color" @click="setColor(0)" style="background-color: black"></div>
+            <div class="controls__color" @click="setColor(1)" style="background-color: white"></div>
+            <div class="controls__color" @click="setColor(2)" style="background-color: #ff3b3c"></div>
+            <div class="controls__color" @click="setColor(3)" style="background-color: #ff9500"></div>
+            <div class="controls__color" @click="setColor(4)" style="background-color: #ffcc00"></div>
+            <div class="controls__color" @click="setColor(5)" style="background-color: #4cd963"></div>
+            <div class="controls__color" @click="setColor(6)" style="background-color: #5ac8fa"></div>
+            <div class="controls__color" @click="setColor(7)" style="background-color: #0579ff"></div>
+            <div class="controls__color" @click="setColor(8)" style="background-color: #5856d6"></div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import { ref, reactive, onMounted, watch } from 'vue'
+
+export default {
+  setup () {
+    const canvas = ref()
+    const data = reactive({
+      painting: false,
+      ctx: 0,
+      colors: {
+        0: 'black',
+        1: 'white',
+        2: '#ff3b3c',
+        3: '#ff9500',
+        4: '#ffcc00',
+        5: '#4cd963',
+        6: '#5ac8fa',
+        7: '#0579ff',
+        8: '#5856d6'
+      },
+      jsRange: 2.5
+    })
+
+    onMounted(() => {
+      prepare()
+    })
+
+    watch(() => {
+      data.jsRange = () => {
+        console.log(data.jsRange)
+      }
+    })
+
+    const prepare = () => {
+      console.log(canvas.value)
+      data.ctx = canvas.value.getContext('2d')
+      //   console.log(data.ctx)
+      data.ctx.strokeStyle = '#2c2c2c'
+      data.ctx.lineWidth = data.jsRange
+
+      console.log(data.ctx)
+    }
+
+    const startPainting = () => {
+      data.paiting = true
+    }
+
+    const stopPainting = () => {
+      data.painting = false
+    }
+
+    const onMouseMove = (event) => {
+    //   console.log(event)
+    //   prepare()
+      const x = event.offsetX
+      const y = event.offsetY
+      console.log(x, y)
+
+      if (!data.painting) {
+        data.ctx.beginPath()
+        data.ctx.moveTo(x, y)
+      } else {
+        data.ctx.lineTo(x, y)
+        data.ctx.stroke()
+      }
+    }
+
+    const onMouseDown = (event) => {
+      data.painting = true
+    //   console.log(canRef.value)
+    }
+
+    const onMouseUp = (event) => {
+      stopPainting()
+    }
+
+    const setColor = (num) => {
+      data.ctx.strokeStyle = data.colors[num]
+    }
+
+    return {
+      startPainting,
+      stopPainting,
+      onMouseMove,
+      onMouseDown,
+      onMouseUp,
+      prepare,
+      setColor,
+      //   handleRangeChange,
+      data,
+      canvas
+    }
+  }
+}
+</script>
+
+<style>
+body {
+    background-color: #f6f9fc;
+    font-family: -apple-system, -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+    'Open Sans', 'Helvetica Neue', sans-serif;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 50px 0px;
+}
+
+.canvas {
+    width: 700px;
+    height: 700px;
+    background-color: white;
+    border-radius: 15px;
+    box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0, 1px 3px rgba(0, 0, 0, 0.08);
+}
+
+.controls {
+    margin-top: flex;
+}
+
+.controls .controls__btns{
+    margin-bottom: 30px;
+}
+
+.controls__btns button {
+    all: unset;
+    cursor: pointer;
+    background-color: white;
+    padding: 5px 0px;
+    width: 80px;
+    text-align: center;
+    border-radius: 5px;
+    box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0,0,0,0.08);
+    border: 2px solid rgba(0, 0, 0, 0.2);
+    color: rgba(0, 0, 0, 0.7);
+    text-transform: uppercase;
+    font-weight: 800;
+    font-size: 12px;
+}
+.controls__btns button:active{
+    transform: scale(0.98)
+}
+
+.controls .controls__colors{
+    display: flex;
+}
+
+.controls__colors .controls__color{
+    width: 50px;
+    height: 50px;
+    border-radius: 25px;
+    cursor: pointer;
+    box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+}
+</style>
