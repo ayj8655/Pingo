a3f1801dde0c8e37399efe98483cacabe26c5ca8
multicampus <ruvan123@naver.com>
2021-09-15 11:08:06 +0900

    [FE] 0915 work until now

--- frontend/src/views/lobby.vue
+++ frontend/src/views/lobby.vue
@@ -1,12 +1,39 @@
-<template>
+ <template>
     <div>
-        <div> <h3>Lobby</h3></div>
+      <div> <h3>Lobby</h3></div>
+      <button @click="createRoom">방만들기</button>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { onMounted } from 'vue'
 export default {
+  name: 'Lobby',
+  components: {
 
+  },
+  setup () {
+    const roomList = []
+    onMounted(() => {
+      axios.get({
+        url: ''
+      }).then(res => {
+        console.log(res)
+      })
+    })
+    const creatRoom = () => {
+      axios.post({
+        url: ''
+        // 보류 socket
+      })
+    }
+
+    return {
+      roomList,
+      creatRoom
+    }
+  }
 }
 </script>
 
