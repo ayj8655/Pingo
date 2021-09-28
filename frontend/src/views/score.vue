9e522b529c52f1acb5754c59ebc1e5f48c3245b5
multicampus <ruvan123@naver.com>
2021-09-28 22:33:03 +0900

    [FE]210928 minor fix

--- frontend/src/views/score.vue
+++ frontend/src/views/score.vue
@@ -9,31 +9,34 @@ import { onMounted, ref } from '@vue/runtime-core'
 import axios from 'axios'
 
 export default {
-  setup() {
-    const score = ref({})
-    const classes = ref({})
-    onMounted(() => {
-      axios({
-        method: 'POST',
-        url: "http://localhost:8000/paint_game/ayj",
-        data: {
+setup(){
+
+  const score = ref({})
+  const classes = ref({})
+  onMounted(() => {
+    axios({
+      method: 'GET',
+      url: "http://localhost:8000/paint_game/ayj",
+      data: {
           'user_name':localStorage.getItem('user_name'),
           'room_id':localStorage.getItem('room_id'),
           'category': 'banana' //여기 나중에 수정
         }
-      })
-      .then((res) => {
-        console.log(res.data)
-        score.value = res.data.score
-        classes.value = res.data.class_name
-      })
+
+
     })
-    return {
-      score,
-      onMounted,
-      classes
-    }
+    .then((res) => {
+      console.log(res.data)
+      score.value = res.data.score
+      classes.value = res.data.class_name
+    })
+  })
+  return {
+    score,
+    onMounted,
+    classes
   }
+}
 }
 </script>
 
