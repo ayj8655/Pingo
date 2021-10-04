3c9a78441b5528641a634a259ed4e9ca697d2d81
KimSunwoo <rlatjsd0603@naver.com>
2021-10-05 01:10:10 +0900

    crontab test complete

--- backend/paint_game/cron.py
+++ backend/paint_game/cron.py
@@ -2,5 +2,5 @@ import subprocess
 
 def update():
     print("업데이트 시작")
-    subprocess.run('sudo mkdir /home/jenkins/workspace/test/backend/crontab',shell=True)
+#    subprocess.run('sudo mkdir /home/jenkins/workspace/test/backend/crontab',shell=True)
     print("업데이트 끗")
