3859d161591dce76b7f16ea9b705729a6be4cdd3
KimSunwoo <rlatjsd0603@naver.com>
2021-10-05 17:37:47 +0900

    cron email test4
    Former-commit-id: edbbe30def938834e0c398e4e64354e9c071d360

--- backend/paint_game/cron.py
+++ backend/paint_game/cron.py
@@ -7,7 +7,7 @@ def update():
     numbers = len(os.listdir(dirpath))
     email = EmailMessage(
         '장고 이메일 테스트',  # 제목
-        '이것은 테스트 이메일 입니다.' + numbers + '개의 파일이 있습니다.',  # 내용
+        '이것은 테스트 이메일 입니다.' + str(numbers) + '개의 파일이 있습니다.',  # 내용
         # 'from@example.com',  # 보내는 이메일 (settings에서 설정해서 작성안해도 됨)
         to=['rlatjsd0603@naver.com'],  # 받는 이메일 리스트
     )
