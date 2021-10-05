8b2d3c49f50a9592bbcce821b6bd24475d00e899
KimSunwoo <rlatjsd0603@naver.com>
2021-10-05 18:32:05 +0900

    hint for crontab email
    Former-commit-id: 62f735e667e10ba5d991318517946ffba39d00dc

--- backend/paint_game/cron.py
+++ backend/paint_game/cron.py
@@ -12,7 +12,7 @@ def update():
         name = '없어'
     email = EmailMessage(
         '장고 이메일 테스트',  # 제목
-        '이것은 테스트 이메일 입니다.' + str(len(numbers)) + '개의 파일이 있습니다.',  # 내용
+        '이것은 테스트 이메일 입니다.' + str(len(numbers)) + '개의 파일이 있습니다.' +os.path.realpath(__file__)+os.path.abspath(__file__),  # 내용
         # 'from@example.com',  # 보내는 이메일 (settings에서 설정해서 작성안해도 됨)
         to=['rlatjsd0603@naver.com'],  # 받는 이메일 리스트
     )
