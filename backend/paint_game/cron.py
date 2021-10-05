e7f2d4a1f3a822fdd8d08cab2bd67843d02d058a
KimSunwoo <rlatjsd0603@naver.com>
2021-10-05 18:24:35 +0900

    crontab email not working. i dont know why
    Former-commit-id: 3649e845e09743d79eb8b11f02d5418d3c852197

--- backend/paint_game/cron.py
+++ backend/paint_game/cron.py
@@ -3,12 +3,16 @@ import os
 
 def update():
     print("업데이트 알림 시작")
-    dirpath = "/home/jenkins/workspace/test/backend/media/"
-    numbers = len(os.listdir(dirpath))
+    dirpath = "./"
+    numbers = os.listdir(dirpath)
     # numbers = '1'
+    if len(numbers) > 0 :
+        name = str(numbers[0])
+    else :
+        name = '없어'
     email = EmailMessage(
         '장고 이메일 테스트',  # 제목
-        '이것은 테스트 이메일 입니다.' + str(numbers) + '개의 파일이 있습니다.',  # 내용
+        '이것은 테스트 이메일 입니다.' + str(len(numbers)) + '개의 파일이 있습니다.',  # 내용
         # 'from@example.com',  # 보내는 이메일 (settings에서 설정해서 작성안해도 됨)
         to=['rlatjsd0603@naver.com'],  # 받는 이메일 리스트
     )
--- backend/paint_game/test.py
+++
@@ -1,6 +0,0 @@
-from django.core.mail import EmailMessage
-import os
-
-dirpath = "../"
-numbers = os.listdir(dirpath)
-print(numbers)
