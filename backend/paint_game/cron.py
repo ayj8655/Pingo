f194a2b51fde07c0f6b2b49df56b9ed02b6109f7
KimSunwoo <rlatjsd0603@naver.com>
2021-10-05 22:33:33 +0900

    zzz
    Former-commit-id: 38fec85e638080f86901af3c36f88b5d7b998dc1

--- backend/paint_game/cron.py
+++ backend/paint_game/cron.py
@@ -22,13 +22,13 @@ def update():
 
     for category in categories :
         if os.path.isdir("/home/jenkins/workspace/test/backend/media/dataset/success/"+category) :
-            amount += os.listdir("/home/jenkins/workspace/test/backend//media/dataset/success/"+category)
+            amount += len(os.listdir("/home/jenkins/workspace/test/backend//media/dataset/success/"+category))
 
-    if True:
+    if True :
         email = EmailMessage(
             '[PINGO] 분류 성공 이미지 누적 안내',  # 제목
-            '이것은 테스트 이메일 입니다. 현재' + now_dir + '에 있습니다',
-            to=['rlatjsd@naver.com'],  # 받는 이메일 리스트
+            '이것은 테스트 이메일 입니다. 현재' + str(now_dir) + '에 있습니다',
+            to=['rlatjsd0603@naver.com'],  # 받는 이메일 리스트
         )
         email.send()
         # os.makedirs("/home/jenkins/workspace/test/backend/media/dataset/success/", exist_ok=True)
