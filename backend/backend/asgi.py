77d552f0c001bcc1fc7d032ea8f79b66242d6f4d
KimSunwoo <rlatjsd0603@naver.com>
2021-10-07 02:22:45 +0900

    asgi success
    Former-commit-id: 665a32e3bcae42d8bd1613c06762cbe730e8e6ef

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -9,21 +9,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
-#from channels.auth import AuthMiddlewareStack #추가
-#from channels.routing import ProtocolTypeRouter, URLRouter #URLRouter 추가
 from channels.routing import get_default_application
-#from paint_game import routing
 import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django.setup()
 
-# application = ProtocolTypeRouter({
-#   "http": get_asgi_application(),
-#   "websocket": AuthMiddlewareStack( # 추가
-#         URLRouter(
-#             routing.websocket_urlpatterns
-#         )
-#     ),
-# })
 application = get_default_application()
--- backend/backend/routing.py
+++ backend/backend/routing.py
@@ -1,4 +1,3 @@
-from django.urls import re_path
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
