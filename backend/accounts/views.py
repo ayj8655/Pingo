c5f6dc80d603be9e43e2342811e911c769eedf17
cht <notime7389@gmail.com>
2021-10-07 20:46:53 +0900

    [be & fe] feature: 회원가입 정규표현검사
    Former-commit-id: 3224161515f81cb5648aa3410f93022802dd3856

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,14 +1,14 @@
 from django.http import JsonResponse
-from django.shortcuts import get_list_or_404, get_object_or_404
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
-import subprocess
-
 from .models import Accounts
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 import datetime
+import re
 
+signReg = re.compile(r'^[0-9a-zA-zㄱ-ㅎㅏ-ㅣ가-힣]{3,8}$')
 # Create your views here.
 
 r_body_user = openapi.Schema(
@@ -24,10 +24,13 @@ def signup(request): #회원가입하고 유저 정보 바로 리턴
     # print("유저 임시 로그인(생성)")
     now = datetime.datetime.now() + datetime.timedelta(hours=1)
     # print('request', request.data)
-    new_user = Accounts.objects.create(user_name=request.data.get('user_name'), time_to_expire=now)
-#    subprocess.run('sudo mkdir crontest', shell=True)
-    return Response({"user_name": new_user.user_name, "user_id":new_user.user_id})
-
+    user_name = request.data.get('user_name')
+    flag = bool(signReg.match(user_name))
+    if flag:
+        new_user = Accounts.objects.create(user_name=user_name, time_to_expire=now)
+        return Response({"user_name": new_user.user_name, "user_id":new_user.user_id})
+    else:
+        return Response({"detail":"invaild input"}, status=status.HTTP_400_BAD_REQUEST)
 @swagger_auto_schema(method='delete', request_body= openapi.Schema(
     type=openapi.TYPE_OBJECT,
     properties={
