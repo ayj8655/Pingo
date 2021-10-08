2f17f5ac8a5918de2bce20e7d78cb836b99800bc
KimSunwoo <rlatjsd0603@naver.com>
2021-10-08 10:44:07 +0900

    cronjob hotfix
    Former-commit-id: 1f716db73801e262c13e139019fe8f38cf9ee8b7

--- backend/paint_game/cron.py
+++ backend/paint_game/cron.py
@@ -23,18 +23,10 @@ def update():
         if os.path.isdir("/home/new_jenkins/jenkins/workspace/k-test/backend/media/dataset/success/"+category) :
             amount += len(os.listdir("/home/new_jenkins/jenkins/workspace/k-test/backend/media/dataset/success/"+category))
 
-    if amount > 1000 :
+    if amount > 100 :
         email = EmailMessage(
             '[PINGO] 분류 성공 이미지 누적 안내',  # 제목
             '현재' + str(amount) + '개의 이미지가 있습니다. 이미지를 GPU 서버로 옮겨서 테스트 해주십시오',
             to=['rlatjsd0603@naver.com'],  # 받는 이메일 리스트
         )
         email.send()
-    else :
-        email = EmailMessage(
-            '[PINGO] 분류 성공 이미지 누적 안내',  # 제목
-            '현재' + str(amount) + '개의 이미지가 있습니다. 이미지가 더 누적되기를 기다려 주세요',
-            to=['rlatjsd0603@naver.com'],  # 받는 이메일 리스트
-        )
-        email.send()
-        # os.makedirs("/home/jenkins/workspace/test/backend/media/dataset/success/", exist_ok=True)
